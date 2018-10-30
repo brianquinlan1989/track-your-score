a8fa031ae32af213fc7c8a6a163737be6a5bfc58
Brian Quinlan <b_quinny@hotmail.com>
2018-10-30 21:57:16 +0000

    updated bashrc file

--- trackyourscore.py
+++ trackyourscore.py
@@ -4,6 +4,10 @@ import os
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
 @app.route("/")
 def show_home():
     return "hi"
