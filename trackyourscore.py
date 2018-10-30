ac983fadc25756e0df1e4892ff372fbed9f8a09c
Brian Quinlan <b_quinny@hotmail.com>
2018-10-30 20:47:24 +0000

    initial commit

---
+++ trackyourscore.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def show_home():
+    return "hi"
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
