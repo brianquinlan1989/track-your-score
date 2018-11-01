4e8631cf50dc2c6dad49e164b352ad1e1f04f7ca
Brian Quinlan <b_quinny@hotmail.com>
2018-11-01 09:24:25 +0000

    added new files

--- trackyourscore.py
+++ trackyourscore.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -7,10 +9,27 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
+mongo = PyMongo(app)
+
+def get_province_names():
+    provinces = []
+    for province in mongo.db.collection_names():
+        if not province.startswith("system."):
+                province.append(province)
+    return provinces
 
 @app.route("/")
 def show_home():
-    return "hi"
+    return render_template("home.html")
+    
+@app.route("/scores/<province>")
+def show_entries_by_province(province):
+    entries = mongo.db[province].find()
+    return render_template("scores.html", entries = entries)
+    
+@app.route("/add_entry", methods=["GET", "POST"])
+def add_entry():
+    return render_template("add_entry.html")
     
     
     
